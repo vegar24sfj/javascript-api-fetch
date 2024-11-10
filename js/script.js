55e9611a75844ac654f40262c7d2629841c2635d
vegar24sfj <vegar24sfj@gmail.com>
2024-11-10 23:24:52 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -3,21 +3,27 @@ const content = document.getElementById("content")
 
 
 async function getData() {
-    const response = await fetch("https://ghibliapi.vercel.app/films")
+
+    // first layer fetch
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=386")
     const data = await response.json()
-    console.log(data)
-    // console.log(data[0].title)
+    // console.log(data.results)
+
+    // second layer fetch
+    const pokemonUrls = data.results.map((pokemon)=> pokemon.url)
+    const pokemonPromises = pokemonUrls.map((url)=> fetch(url).then((response)=> response.json()))
+    const pokeDetails = await Promise.all(pokemonPromises);
+
+    // console.log(pokemonUrls)
     // const allTitles = data.map((movie) => {
-    //     console.log(movie.title)
-    // })
+    // console.log(movie.title)
 
-
-    content.innerHTML = (data.map((movie) => {
+    content.innerHTML = (pokeDetails.map((pokemon) => {
         return (
             `
-        <div class="movie-card"> 
-          <h2>${movie.title}</h2>
-          <img src="${movie.image}" />
+        <div class="pokemon-card"> 
+          <h2>${pokemon.name}</h2>
+          <img src="${pokemon.sprites.front_default}"/>
         </div>
         
         
