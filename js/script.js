c45e7791633b5d5605de444b1003dfcfc69f6745
vegar24sfj <vegar24sfj@gmail.com>
2024-11-13 11:27:38 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
-const content = document.getElementById("content")
-
-
+const content = document.getElementById("content");
 
 async function getData() {
+  // first layer fetch
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=386");
+  const data = await response.json();
+  // console.log(data.results)
 
-    // first layer fetch
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=386")
-    const data = await response.json()
-    // console.log(data.results)
+  // second layer fetch
+  const pokemonUrls = data.results.map((pokemon) => pokemon.url);
+  const pokemonPromises = pokemonUrls.map((url) =>
+    fetch(url).then((response) => response.json())
+  );
+  const pokeDetails = await Promise.all(pokemonPromises);
 
-    // second layer fetch
-    const pokemonUrls = data.results.map((pokemon)=> pokemon.url)
-    const pokemonPromises = pokemonUrls.map((url)=> fetch(url).then((response)=> response.json()))
-    const pokeDetails = await Promise.all(pokemonPromises);
+  // console.log(pokemonUrls)
+  // const allTitles = data.map((movie) => {
+  // console.log(movie.title)
 
-    // console.log(pokemonUrls)
-    // const allTitles = data.map((movie) => {
-    // console.log(movie.title)
-
-    content.innerHTML = (pokeDetails.map((pokemon) => {
-        return (
-            `
+  content.innerHTML = pokeDetails
+    .map((pokemon) => {
+      return `
         <div class="pokemon-card"> 
           <h2>${pokemon.name}</h2>
           <img src="${pokemon.sprites.front_default}"/>
         </div>
         
         
-        `
-        )
-    }).join(" "))
-
+        `;
+    })
+    .join(" ");
 }
-getData()
+getData();
